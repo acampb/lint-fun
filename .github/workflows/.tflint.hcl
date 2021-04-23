bd244a577890da83068031cd6787947f4aaa41d9
Andrew Campbell <andrewbcampbell@outlook.com>
2021-04-23 16:20:27 -0400

    add tflint config

---
+++ .github/workflows/.tflint.hcl
@@ -0,0 +1,18 @@
+config {
+  module = true
+  force = false
+  disabled_by_default = false
+
+  #ignore_module = {
+  #  "terraform-aws-modules/vpc/aws"            = true
+  #  "terraform-aws-modules/security-group/aws" = true
+  #}
+
+  #varfile = ["example1.tfvars", "example2.tfvars"]
+  #variables = ["foo=bar", "bar=[\"baz\"]"]
+}
+
+
+plugin "azurerm" {
+  enabled = true
+}
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -4,8 +4,15 @@ resource "azurerm_storage_account" "example" {
   location                 = "East US 2"
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  allow_blob_public_access = true
 
   tags = {
     environment = "staging"
   }
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "vhds"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "container"
 }
