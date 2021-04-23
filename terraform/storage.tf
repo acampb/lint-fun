690fb3f90ff6ee99e36967c9a8e585f18a204fc2
Andrew Campbell <andrewbcampbell@outlook.com>
2021-04-23 16:23:16 -0400

    upgrade storage

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -3,7 +3,7 @@ resource "azurerm_storage_account" "example" {
   resource_group_name      = "rg-notreal"
   location                 = "East US 2"
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "ARS"
   allow_blob_public_access = true
 
   tags = {
@@ -14,5 +14,5 @@ resource "azurerm_storage_account" "example" {
 resource "azurerm_storage_container" "example" {
   name                  = "vhds"
   storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "container"
+  container_access_type = "private"
 }
