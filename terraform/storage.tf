3bfae5f337ee1b1aff8c37ede1b8e51a9b633947
Andrew Campbell <andrewbcampbell@outlook.com>
2021-04-23 16:26:34 -0400

    more opens

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -14,5 +14,5 @@ resource "azurerm_storage_account" "example" {
 resource "azurerm_storage_container" "example" {
   name                  = "vhds"
   storage_account_name  = azurerm_storage_account.example.name
-  container_access_type = "private"
+  container_access_type = "container"
 }
